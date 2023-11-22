caf7759eb8cb3ada6a43afd1377586675aed9865
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-22 10:57:01 +0300

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System;
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: "); int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: " + name);
+            Console.WriteLine("Ваш возраст: " + age);
+            Console.Title = "Ваше имя и возраст";
+            string txt = "Ваше имя: " + name + " Ваш возраст:  " + age;
+            Console.WriteLine(txt);
+            Console.ReadKey();
+         }
+    }
+}
+//namespace UserInput
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name = Interaction.InputBox("Введите ваше имя:", "Имя", "");
+//            string ageString = Interaction.InputBox("Введите ваш возраст:", "Возраст", ""); 
+//            int age = Convert.ToInt32(ageString); 
+//            MessageBox.Show("Ваше имя: " + name + " Ваш возраст: " + age, "Информация");
+//        }
+//    }
+//}
---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace InputDialogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, fam;
+            Console.Title = "Имя и фамилия" ;
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            fam = Console.ReadLine();
+            Console.Title = "Ваша имя и фамилия";
+            string txt = "Ваша имя: " + name + " Ваша фамилия: " + fam;
+            Console.WriteLine(txt);
+            Console.ReadKey();
+        }
+    }
+}
