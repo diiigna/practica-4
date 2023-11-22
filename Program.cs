22b403d5031f7a89111bdcb0ef94414e93bad914
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-22 10:49:57 +0300

    Пратика 2

--- Program.cs
+++ Program.cs
@@ -5,22 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
-namespace InputDialogDemo
+using System;
+namespace UserInput
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name, fam;
-            Console.Title = "Имя и фамилия" ;
             Console.Write("Введите ваше имя: ");
-            name = Console.ReadLine();
-            Console.Write("Введите вашу фамилию: ");
-            fam = Console.ReadLine();
-            Console.Title = "Ваша имя и фамилия";
-            string txt = "Ваша имя: " + name + " Ваша фамилия: " + fam;
+            string name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: "); int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: " + name);
+            Console.WriteLine("Ваш возраст: " + age);
+            Console.Title = "Ваше имя и возраст";
+            string txt = "Ваше имя: " + name + " Ваш возраст:  " + age;
             Console.WriteLine(txt);
             Console.ReadKey();
-        }
+         }
     }
 }
+//namespace UserInput
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name = Interaction.InputBox("Введите ваше имя:", "Имя", "");
+//            string ageString = Interaction.InputBox("Введите ваш возраст:", "Возраст", ""); 
+//            int age = Convert.ToInt32(ageString); 
+//            MessageBox.Show("Ваше имя: " + name + " Ваш возраст: " + age, "Информация");
+//        }
+//    }
+//}
